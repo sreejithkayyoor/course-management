2ec202c1eefdd7bf20b550d13b12961bbd9883c2
Sreejith <sreejithkayyoor@gmail.com>
2020-04-19 11:50:55 +0530

    Pages with nav bar

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Plurasight Administration</h1>
+      <p>React Flux and React Router for ultra-responsive web apps.</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+export default HomePage;
